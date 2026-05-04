50cc3a79139357da513d047c8ca88107ee2b8509
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2026-05-04 21:42:12 +0900

    Sceneの入力検証を追加

--- resonite_link_ex/lib/resonite_link_ex/scene.ex
+++ resonite_link_ex/lib/resonite_link_ex/scene.ex
@@ -3,11 +3,21 @@ defmodule ResoniteLinkEx.Scene do
   ResoniteLink 命令呼び出しの入口API。
   """
 
+  alias ResoniteLinkEx.Protocol
+
   @doc """
   指定した `$type` と `payload` で命令を呼び出す。
   """
   @spec call(term(), String.t(), map()) :: {:ok, map()} | {:error, term()}
+  def call(_client, type, payload) when is_binary(type) and is_map(payload) do
+    if Protocol.valid_type?(type) do
+      {:error, :not_implemented}
+    else
+      {:error, :invalid_request}
+    end
+  end
+
   def call(_client, _type, _payload) do
-    {:error, :not_implemented}
+    {:error, :invalid_request}
   end
 end
--- resonite_link_ex/test/resonite_link_ex/scene_test.exs
+++ resonite_link_ex/test/resonite_link_ex/scene_test.exs
@@ -6,4 +6,12 @@ defmodule ResoniteLinkEx.SceneTest do
   test "call/3 は現時点で not_implemented を返す" do
     assert {:error, :not_implemented} = Scene.call(:client, "requestSessionData", %{})
   end
+
+  test "call/3 は許可リスト外の type で invalid_request を返す" do
+    assert {:error, :invalid_request} = Scene.call(:client, "unknownType", %{})
+  end
+
+  test "call/3 は payload が map 以外なら invalid_request を返す" do
+    assert {:error, :invalid_request} = Scene.call(:client, "requestSessionData", [])
+  end
 end
