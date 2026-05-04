9578e60602a0539ddd1ad1591e14936d538d75a6
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2026-05-05 01:13:02 +0900

    Sceneの対応コマンド拡張と分岐テストを追加

--- resonite_link_ex/lib/resonite_link_ex/scene.ex
+++ resonite_link_ex/lib/resonite_link_ex/scene.ex
@@ -3,10 +3,17 @@ defmodule ResoniteLinkEx.Scene do
   ResoniteLink 命令呼び出しの入口API。
   """
 
+  alias ResoniteLinkEx.Protocol
+
   @invalid_request {:error, :invalid_request}
   @not_implemented {:error, :not_implemented}
   @type_request_session_data "requestSessionData"
   @type_add_slot "addSlot"
+  @type_update_slot "updateSlot"
+  @type_add_component "addComponent"
+  @type_update_component "updateComponent"
+  @type_remove_component "removeComponent"
+  @type_remove_slot "removeSlot"
 
   @doc """
   指定した `$type` と `payload` で命令を呼び出す。
@@ -24,5 +31,24 @@ defmodule ResoniteLinkEx.Scene do
     do: @not_implemented
 
   def call(_client, @type_add_slot, _payload), do: @invalid_request
+
+  def call(_client, @type_update_slot, payload),
+    do: map_result(Protocol.validate_payload(@type_update_slot, payload))
+
+  def call(_client, @type_add_component, payload),
+    do: map_result(Protocol.validate_payload(@type_add_component, payload))
+
+  def call(_client, @type_update_component, payload),
+    do: map_result(Protocol.validate_payload(@type_update_component, payload))
+
+  def call(_client, @type_remove_component, payload),
+    do: map_result(Protocol.validate_payload(@type_remove_component, payload))
+
+  def call(_client, @type_remove_slot, payload),
+    do: map_result(Protocol.validate_payload(@type_remove_slot, payload))
+
   def call(_client, _type, _payload), do: @invalid_request
+
+  defp map_result({:ok, _payload}), do: @not_implemented
+  defp map_result(_error), do: @invalid_request
 end
--- resonite_link_ex/test/resonite_link_ex/scene_test.exs
+++ resonite_link_ex/test/resonite_link_ex/scene_test.exs
@@ -32,4 +32,51 @@ defmodule ResoniteLinkEx.SceneTest do
   test "call/3 は type が文字列でない場合に invalid_request を返す" do
     assert {:error, :invalid_request} = Scene.call(:client, :add_slot, %{})
   end
+
+  test "call/3 は updateSlot の必須条件が揃っていれば not_implemented を返す" do
+    payload = %{slot_id: "SlotA", position: %{x: 0, y: 1, z: 0}}
+    assert {:error, :not_implemented} = Scene.call(:client, "updateSlot", payload)
+  end
+
+  test "call/3 は addComponent の必須条件が揃っていれば not_implemented を返す" do
+    payload = %{slot_id: "SlotA", component_type: "FrooxEngine.BoxCollider"}
+    assert {:error, :not_implemented} = Scene.call(:client, "addComponent", payload)
+  end
+
+  test "call/3 は addComponent の必須条件不足で invalid_request を返す" do
+    payload = %{slot_id: "SlotA"}
+    assert {:error, :invalid_request} = Scene.call(:client, "addComponent", payload)
+  end
+
+  test "call/3 は updateComponent の必須条件が揃っていれば not_implemented を返す" do
+    payload = %{component_id: "CompA", members: %{"Enabled" => true}}
+    assert {:error, :not_implemented} = Scene.call(:client, "updateComponent", payload)
+  end
+
+  test "call/3 は updateComponent の必須条件不足で invalid_request を返す" do
+    payload = %{component_id: "CompA"}
+    assert {:error, :invalid_request} = Scene.call(:client, "updateComponent", payload)
+  end
+
+  test "call/3 は removeComponent の必須条件が揃っていれば not_implemented を返す" do
+    payload = %{component_id: "CompA"}
+    assert {:error, :not_implemented} = Scene.call(:client, "removeComponent", payload)
+  end
+
+  test "call/3 は removeComponent の必須条件不足で invalid_request を返す" do
+    assert {:error, :invalid_request} = Scene.call(:client, "removeComponent", %{})
+  end
+
+  test "call/3 は removeSlot の必須条件が揃っていれば not_implemented を返す" do
+    payload = %{slot_id: "SlotA"}
+    assert {:error, :not_implemented} = Scene.call(:client, "removeSlot", payload)
+  end
+
+  test "call/3 は removeSlot の必須条件不足で invalid_request を返す" do
+    assert {:error, :invalid_request} = Scene.call(:client, "removeSlot", %{})
+  end
+
+  test "call/3 は updateSlot の必須条件不足で invalid_request を返す" do
+    assert {:error, :invalid_request} = Scene.call(:client, "updateSlot", %{slot_id: "SlotA"})
+  end
 end
