de287e68c7ce9c16d6091c99efc89aeca5866265
ymn-t-yamanashi <ae86bg5@yahoo.co.jp>
2026-05-05 01:21:52 +0900

    Clientに接続状態判定APIを追加

--- resonite_link_ex/lib/resonite_link_ex/client.ex
+++ resonite_link_ex/lib/resonite_link_ex/client.ex
@@ -13,6 +13,13 @@ defmodule ResoniteLinkEx.Client do
     GenServer.start_link(__MODULE__, opts, [])
   end
 
+  @doc """
+  クライアントプロセスが生存していれば `true` を返す。
+  """
+  @spec connected?(pid()) :: boolean()
+  def connected?(pid) when is_pid(pid), do: Process.alive?(pid)
+  def connected?(_pid), do: false
+
   @impl true
   @doc """
   クライアントの初期状態を構築する。
--- resonite_link_ex/test/resonite_link_ex/client_test.exs
+++ resonite_link_ex/test/resonite_link_ex/client_test.exs
@@ -7,7 +7,21 @@ defmodule ResoniteLinkEx.ClientTest do
     assert {:ok, pid} = Client.start_link([])
     assert is_pid(pid)
     assert Process.alive?(pid)
+    assert Client.connected?(pid)
 
     Process.exit(pid, :normal)
+  end
+
+  test "connected?/1 は停止済みプロセスで false を返す" do
+    assert {:ok, pid} = Client.start_link([])
+    Process.unlink(pid)
+    Process.exit(pid, :shutdown)
+    Process.sleep(10)
+
+    refute Client.connected?(pid)
+  end
+
+  test "connected?/1 は pid 以外で false を返す" do
+    refute Client.connected?(:not_pid)
   end
 end
